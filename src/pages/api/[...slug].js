f4aea7dd2a4ca62573c846ee202d28ca0c7b9223
Ghadge Shubham <87597227+ghadges@users.noreply.github.com>
2025-01-30 23:21:37 +0000

    Fix import paths and package manager

--- src/pages/api/[...slug].js
+++ src/pages/api/[...slug].js
@@ -1,4 +1,4 @@
-import { supabase } from "../../../lib/supabase-client";
+import { supabase } from "../../lib/supabase-client";
 
 export default async function handler(req, res) {
   const { slug } = req.query;
